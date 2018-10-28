94b6b1c221dedfaca8d2e780044b53775eb6d0cb
elizabith.costey <costey.ea@gmail.com>
2018-10-28 17:41:22 -0400

    First Commit- setting up boards

---
+++ src/makeBoard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function makeBoard(props){
+for (let i = 0; i < arrTiles.length; i += 1) {
+    for (let j = 0; j < arrTiles.length; j += 1) {
+        const tileDivs = document.createElement('div');
+        const currentClassName = arrTiles[i][j];
+        tileDivs.className = 'tile';
+        tileDivs.classList.add(currentClassName);
+        tileDivs.id = currentClassName + j + i;
+        board1.appendChild(tileDivs);
+    }
+}
+}
+
+export default makeBoard;
