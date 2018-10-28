bff1c458fc5bcab9adcc20473ec10cc658df810a
elizabith.costey <costey.ea@gmail.com>
2018-10-28 18:14:07 -0400

    Created LandingPage file, started switch statement in App.jsx, and started setting up state with 'currentview'.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
-import makeBoard from './makeBoard';
+import makeBoard from './Board';
 import './App.css';
+import LandingPage from './LandingPage';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentView: 'LandingPage',
+    }
+  }
+
+  switchView() {
+    const { currentView } = this.state;
+    switch (currentView) {
+      case 'LandingPage':
+        return <LandingPage />
+      case 'Lvl1':
+        return <Board />
+      case 'Lvl2':
+        return <Board />
+    }
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
---
+++ src/Board.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+function Board(props) {
+    for (let i = 0; i < this.props.tiles.length; i += 1) {
+        for (let j = 0; j < this.props.tiles.length; j += 1) {
+          const tileDivs = 
+        }
+      }
+    const element = (
+      <div>
+        
+      </div>
+    );
+    render();
+  }
+
+
+
+
+
+
+
+function makeBoard(props){
+for (let i = 0; i < arrTiles.length; i += 1) {
+    for (let j = 0; j < arrTiles.length; j += 1) {
+//         const tileDivs = document.createElement('div');
+//         const currentClassName = arrTiles[i][j];
+//         tileDivs.className = 'tile';
+//         tileDivs.classList.add(currentClassName);
+//         tileDivs.id = currentClassName + j + i;
+//         board1.appendChild(tileDivs);
+//     }
+// }
+}
+
+export default Board;
